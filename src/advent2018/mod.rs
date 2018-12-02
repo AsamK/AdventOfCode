812d259edf59349243b8e00185dc6295bd27020b
Sebastian <asamk@gmx.de>
2018-12-02 13:36:08 +0100

    Add clap command line parser

--- src/advent2018/mod.rs
+++ src/advent2018/mod.rs
@@ -5,20 +5,18 @@ mod day01;
 mod day02;
 
 pub fn get_result<T: Read + BufRead>(data: T, day: u8, level: u8) -> ACResult<String> {
-    let lines = crate::utils::read_lines(data)?;
-
     match day {
         1 => match level {
-            1 => day01::a01_1(lines),
-            2 => day01::a01_2(lines),
+            1 => day01::a01_1(crate::utils::read_lines(data)?),
+            2 => day01::a01_2(crate::utils::read_lines(data)?),
             _ => Err(Error::new(format!(
                 "Level {} not implemented for day {}",
                 level, day
             ))),
         }.map(|r| r.to_string()),
         2 => match level {
-            1 => day02::a02_1(lines).map(|r| r.to_string()),
-            2 => day02::a02_2(lines),
+            1 => day02::a02_1(crate::utils::read_lines(data)?).map(|r| r.to_string()),
+            2 => day02::a02_2(crate::utils::read_lines(data)?),
             _ => Err(Error::new(format!(
                 "Level {} not implemented for day {}",
                 level, day
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,82 @@
+use chrono::Datelike;
+use clap::{App, Arg};
 use errors::{ACResult, Error};
 
 mod advent2018;
 mod errors;
 mod utils;
 
+#[macro_use]
+extern crate clap;
+
+extern crate chrono;
+
 fn main() -> ACResult<()> {
-    let day: u8 = std::env::args()
-        .nth(1)
-        .expect("no day given")
-        .parse()
-        .map_err(|_| Error::new_str("Failed to parse day"))?;
-    let level: u8 = std::env::args()
-        .nth(2)
-        .expect("no level given")
-        .parse()
-        .map_err(|_| Error::new_str("Failed to parse level"))?;
+    let matches = App::new("Advent solver")
+        .version("0.1.0")
+        .author("AsamK <asamk@gmx.de>")
+        .about("Solves puzzles")
+        .arg(
+            Arg::with_name("year")
+                .short("y")
+                .long("year")
+                .value_name("YEAR")
+                .help("Choose the advent year (default: current year)")
+                .requires("day")
+                .takes_value(true),
+        ).arg(
+            Arg::with_name("day")
+                .short("d")
+                .long("day")
+                .value_name("DAY")
+                .help("Choose the advent day (default: current day)")
+                .takes_value(true),
+        ).arg(
+            Arg::with_name("level")
+                .short("l")
+                .long("level")
+                .value_name("LEVEL")
+                .required(true)
+                .help("Choose the advent year")
+                .takes_value(true),
+        ).arg(
+            Arg::with_name("verbose")
+                .short("v")
+                .long("verbose")
+                .help("Make output more verbose"),
+        ).get_matches();
+
+    let hour = 3600;
+    // Advent website is in TimeZone EST/UTC-5
+    let timezone = chrono::FixedOffset::west(5 * hour);
+
+    let year = if matches.is_present("year") {
+        value_t!(matches.value_of("year"), i32).unwrap_or_else(|e| e.exit())
+    } else {
+        chrono::Local::now().with_timezone(&timezone).year()
+    };
+
+    let day = if matches.is_present("day") {
+        value_t!(matches.value_of("day"), u8).unwrap_or_else(|e| e.exit())
+    } else {
+        chrono::Local::now().with_timezone(&timezone).day() as u8
+    };
+
+    let level = value_t!(matches.value_of("level"), u8).unwrap_or_else(|e| e.exit());
+
+    let verbose = matches.is_present("verbose");
+
+    if verbose {
+        eprintln!("Solving puzzle for {} day {} level {}", year, day, level);
+    }
 
     let stdin = std::io::stdin();
     let data = stdin.lock();
 
-    let result = advent2018::get_result(data, day, level)?;
+    let result = match year {
+        2018 => advent2018::get_result(data, day, level),
+        _ => Err(Error::new(format!("Year {} is not implemented", year))),
+    }?;
 
     println!("{}", result);
 
