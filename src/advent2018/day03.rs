d63538955ec59d1559c1a1bfd1d98c48efd6b6ef
Sebastian <asamk@gmx.de>
2018-12-03 08:27:35 +0100

    Implement 2018 day 03

---
+++ src/advent2018/day03.rs
@@ -0,0 +1,161 @@
+use errors::{ACResult, Error};
+use std::io::BufRead;
+
+pub fn get_result<T: BufRead>(data: T, level: u8) -> ACResult<String> {
+    match level {
+        1 => level_1(crate::utils::read_lines(data)?).map(|r| r.to_string()),
+        2 => level_2(crate::utils::read_lines(data)?).map(|r| r.to_string()),
+        _ => Err(Error::new(format!("Level {} not implemented", level))),
+    }
+}
+
+struct FabricPieceInfo {
+    i: usize,
+    left: usize,
+    top: usize,
+    width: usize,
+    height: usize,
+}
+
+named!(number<nom::types::CompleteStr, usize>, flat_map!(complete!(take_while!(|c: char| {c.is_digit(10)})), parse_to!(usize)));
+
+named!(info_line<nom::types::CompleteStr, FabricPieceInfo>,
+  dbg!(do_parse!(
+      tag!("#") >>
+    i: dbg!(number) >>
+      tag!(" @ ") >>
+    left: number >>
+      tag!(",") >>
+    top: number >>
+      tag!(": ") >>
+    width: number >>
+      tag!("x") >>
+    height: number >>
+    (FabricPieceInfo {i, left, top, width, height})
+  ))
+);
+
+struct FabricPiece {
+    inner: Vec<bool>,
+}
+impl FabricPiece {
+    fn new(max_width: usize, max_height: usize, info: &FabricPieceInfo) -> Self {
+        let mut fab = vec![false; max_width * max_height];
+        for y in info.top..(info.top + info.height) {
+            let line_offset = y * max_width;
+            for x in info.left..(info.left + info.width) {
+                fab[line_offset + x] = true;
+            }
+        }
+        FabricPiece { inner: fab }
+    }
+
+    fn get(&self, i: usize) -> bool {
+        self.inner[i]
+    }
+}
+
+fn get_max_width_height((w, h): (usize, usize), info: &FabricPieceInfo) -> (usize, usize) {
+    (
+        if info.left + info.width > w {
+            info.left + info.width
+        } else {
+            w
+        },
+        if info.top + info.height > h {
+            info.top + info.height
+        } else {
+            h
+        },
+    )
+}
+
+fn level_1(lines: Vec<String>) -> ACResult<usize> {
+    let infos = lines
+        .iter()
+        .map(|line| info_line(nom::types::CompleteStr(&line)).map(|x| x.1))
+        .collect::<Result<Vec<FabricPieceInfo>, _>>()
+        .map_err(|_| Error::new_str("Failed to parse blueprint"))?;
+
+    let (max_width, max_height) = infos.iter().fold((0, 0), get_max_width_height);
+
+    let mut overlap = vec![0; max_width * max_height];
+
+    for a in infos.iter() {
+        let fab_a = FabricPiece::new(max_width, max_height, a);
+
+        for b in infos.iter() {
+            if a.i >= b.i
+                || a.left + a.width - 1 < b.left
+                || a.left > b.left + b.width - 1
+                || a.top + a.height - 1 < b.top
+                || a.top > b.top + b.height - 1
+            {
+                continue;
+            }
+            let fab_b = FabricPiece::new(max_width, max_height, b);
+
+            for i in 0..(max_height * max_width) {
+                if fab_a.get(i) && fab_b.get(i) {
+                    overlap[i] += 1;
+                }
+            }
+        }
+    }
+    let mut o = 0;
+    for i in 0..(max_height * max_width) {
+        if overlap[i] >= 1 {
+            o += 1;
+        }
+    }
+    Ok(o)
+}
+
+fn level_2(lines: Vec<String>) -> ACResult<usize> {
+    let infos = lines
+        .iter()
+        .map(|line| info_line(nom::types::CompleteStr(&line)).map(|x| x.1))
+        .collect::<Result<Vec<FabricPieceInfo>, _>>()
+        .map_err(|_| Error::new_str("Failed to parse blueprint"))?;
+
+    let (max_width, max_height) = infos.iter().fold((0, 0), get_max_width_height);
+
+    let mut ov = vec![false; infos.len()];
+
+    for a in infos.iter() {
+        let fab_a = FabricPiece::new(max_width, max_height, a);
+
+        for b in infos.iter() {
+            if a.i >= b.i
+                || a.left + a.width - 1 < b.left
+                || a.left > b.left + b.width - 1
+                || a.top + a.height - 1 < b.top
+                || a.top > b.top + b.height - 1
+            {
+                continue;
+            }
+            let fab_b = FabricPiece::new(max_width, max_height, b);
+
+            for i in 0..(max_height * max_width) {
+                if fab_a.get(i) && fab_b.get(i) {
+                    ov[a.i - 1] = true;
+                    ov[b.i - 1] = true;
+                }
+            }
+        }
+    }
+    let mut overlap_free_i = None;
+    for i in 0..(lines.len()) {
+        if ov[i] == false {
+            if let Some(_) = overlap_free_i {
+                return Err(Error::new_str("Two overlap free pieces found"));
+            }
+            overlap_free_i = Some(i + 1);
+        }
+    }
+    if let Some(i) = overlap_free_i {
+        Ok(i)
+    } else {
+        Err(Error::new_str("No overlap free piece found"))
+    }
+}
--- src/advent2018/mod.rs
+++ src/advent2018/mod.rs
@@ -3,11 +3,13 @@ use std::io::{BufRead, Read};
 
 mod day01;
 mod day02;
+mod day03;
 
 pub fn get_result<T: Read + BufRead>(data: T, day: u8, level: u8) -> ACResult<String> {
     match day {
         1 => day01::get_result(data, level),
         2 => day02::get_result(data, level),
+        3 => day03::get_result(data, level),
         _ => Err(Error::new(format!("Day {} not implemented", day))),
     }
 }
