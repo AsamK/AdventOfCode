1ff6ffce78e64c96825b8d828047313a50b5d794
Sebastian <asamk@gmx.de>
2018-12-07 18:37:15 +0200

    Implement 2017 day 01

---
+++ src/advent2017/day01.rs
@@ -0,0 +1,47 @@
+use errors::{ACResult, Error};
+use std::io::BufRead;
+
+pub fn get_result<T: BufRead>(data: T, level: u8) -> ACResult<String> {
+    match level {
+        1 => level_1(&crate::utils::read_lines(data)?[0]),
+        // 2 => level_2(crate::utils::read_lines(data)?),
+        _ => Err(Error::new(format!("Level {} not implemented", level))),
+    }.map(|r| r.to_string())
+}
+
+fn level_1(line: &str) -> ACResult<u32> {
+    let mut first = Option::None::<u32>;
+    let mut previous = Option::None::<u32>;
+    let mut sum = 0;
+    for c in line.chars() {
+        let digit = c.to_string().parse();
+        match digit {
+            Err(_) => {}
+            Ok(digit) => {
+                match first {
+                    Some(_) => {}
+                    None => {
+                        first = Some(digit);
+                    }
+                }
+                match previous {
+                    Some(p) => {
+                        if p == digit {
+                            sum += p;
+                        }
+                    }
+                    None => {}
+                }
+                previous = Some(digit);
+            }
+        }
+    }
+
+    let c = previous.unwrap();
+    let p = first.unwrap();
+
+    if p == c {
+        sum += p;
+    }
+    Ok(sum)
+}
--- src/advent2017/mod.rs
+++ src/advent2017/mod.rs
@@ -1,10 +1,12 @@
 use crate::errors::{ACResult, Error};
 use std::io::{BufRead, Read};
 
+mod day01;
 mod day25;
 
 pub fn get_result<T: Read + BufRead>(data: T, day: u8, level: u8) -> ACResult<String> {
     match day {
+        01 => day01::get_result(data, level),
         25 => day25::get_result(data, level),
         _ => Err(Error::new(format!("Day {} not implemented", day))),
     }
