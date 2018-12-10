ec9e0e78dd7c04a13114006a3e37d5e27b8bb885
Sebastian <asamk@gmx.de>
2018-12-10 19:28:53 +0100

    Implement 2017 day 01 level 2

--- src/advent2017/day01.rs
+++ src/advent2017/day01.rs
@@ -4,45 +4,54 @@ use std::io::BufRead;
 pub fn get_result<T: BufRead>(data: T, level: u8) -> ACResult<String> {
     match level {
         1 => level_1(&crate::utils::read_lines(data)?[0]),
-        // 2 => level_2(crate::utils::read_lines(data)?),
+        2 => level_2(&crate::utils::read_lines(data)?[0]),
         _ => Err(Error::new(format!("Level {} not implemented", level))),
     }
     .map(|r| r.to_string())
 }
 
 fn level_1(line: &str) -> ACResult<u32> {
-    let mut first = Option::None::<u32>;
-    let mut previous = Option::None::<u32>;
-    let mut sum = 0;
-    for c in line.chars() {
-        let digit = c.to_string().parse();
-        match digit {
-            Err(_) => {}
-            Ok(digit) => {
-                match first {
-                    Some(_) => {}
-                    None => {
-                        first = Some(digit);
-                    }
-                }
-                match previous {
-                    Some(p) => {
-                        if p == digit {
-                            sum += p;
-                        }
-                    }
-                    None => {}
-                }
-                previous = Some(digit);
-            }
+    let numbers = line
+        .chars()
+        .map(|c| {
+            c.to_string()
+                .parse()
+                .map_err(|_| Error::new_str("Failed to parse char to number"))
+        })
+        .collect::<ACResult<Vec<u8>>>()?;
+
+    let mut sum: u32 = 0;
+    for (i, digit) in numbers.iter().enumerate() {
+        let comp_index = (i + 1) % numbers.len();
+        let p = numbers.get(comp_index).unwrap();
+        if *p == *digit {
+            sum += *p as u32;
         }
     }
 
-    let c = previous.unwrap();
-    let p = first.unwrap();
+    Ok(sum)
 
-    if p == c {
-        sum += p;
+}
+
+fn level_2(line: &str) -> ACResult<u32> {
+    let numbers = line
+        .chars()
+        .map(|c| {
+            c.to_string()
+                .parse()
+                .map_err(|_| Error::new_str("Failed to parse char to number"))
+        })
+        .collect::<ACResult<Vec<u8>>>()?;
+
+    let mut sum: u32 = 0;
+    let skip = numbers.len() / 2;
+    for (i, digit) in numbers.iter().enumerate() {
+        let comp_index = (i + skip) % numbers.len();
+        let p = numbers.get(comp_index).unwrap();
+        if *p == *digit {
+            sum += *p as u32;
+        }
     }
+
     Ok(sum)
 }
