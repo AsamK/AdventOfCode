d547442a7e57a5d3a6335d3608327dbee2a9f7fe
Sebastian <asamk@gmx.de>
2018-12-26 21:06:19 +0100

    Improve 2018 day 11
    Cache lines sums
    1m -> 1s

--- src/advent2018/day11.rs
+++ src/advent2018/day11.rs
@@ -12,22 +12,12 @@ pub fn get_result<T: BufRead>(data: T, level: u8) -> ACResult<String> {
 fn level_1(lines: &[String]) -> ACResult<String> {
     let serial: i32 = lines[0].parse().unwrap();
 
-    let size = 300;
+    let size: u32 = 300;
 
-    let mut grid = Vec::new();
-
-    for x in 1..=size {
-        for y in 1..=size {
-            let rack_id = x + 10;
-            let power = (rack_id * y + serial) * rack_id;
-            let power = (power / 100) % 10 - 5;
-            grid.push(power);
-        }
-    }
-    let grid = grid;
+    let grid = new_grid(size, serial);
     let mut squares = Vec::new();
-    for x in 1..=300 - 2 {
-        for y in 1..=300 - 2 {
+    for x in 1..=size - 2 {
+        for y in 1..=size - 2 {
             let square_power = get(&grid, x, y)
                 + get(&grid, x + 1, y)
                 + get(&grid, x + 2, y)
@@ -42,10 +32,24 @@ fn level_1(lines: &[String]) -> ACResult<String> {
     }
     let (i, _max) = squares.iter().enumerate().max_by_key(|&(_, l)| l).unwrap();
 
-    let x = i % 298 + 1;
-    let y = i / 298 + 1;
+    let x = i as u32 % (size - 2) + 1;
+    let y = i as u32 / (size - 2) + 1;
 
     Ok(format!("{},{}", x, y))
+}
+
+fn new_grid(size: u32, serial: i32) -> Vec<i32> {
+    let mut grid = Vec::new();
+
+    for x in 1..=size {
+        for y in 1..=size {
+            let rack_id = x as i32 + 10;
+            let power = (rack_id * y as i32 + serial) * rack_id;
+            let power = (power / 100) % 10 - 5;
+            grid.push(power);
+        }
+    }
+    grid
 }
 
 fn get(grid: &[i32], x: u32, y: u32) -> i32 {
@@ -68,39 +72,29 @@ fn level_2(lines: &[String]) -> ACResult<String> {
 
     let size = 300;
 
-    let mut grid = Vec::new();
-
-    for x in 1..=size {
-        for y in 1..=size {
-            let rack_id = x + 10;
-            let power = (rack_id * y + serial) * rack_id;
-            let power = (power / 100) % 10 - 5;
-            grid.push(power);
-        }
-    }
-    let grid = grid;
+    let grid = new_grid(size, serial);
 
     let mut max = i32::min_value();
     let mut result = "".to_owned();
 
-    for s in 1..=300 {
-        let mut squares = Vec::new();
-        for x in 1..=300 - s + 1 {
-            for y in 1..=300 - s + 1 {
-                let mut square_power = 0;
-                for sx in 0..s {
-                    for sy in 0..s {
-                        square_power += get(&grid, x + sx, y + sy);
-                    }
-                }
-                squares.push(square_power);
-            }
-        }
-        let (i, smax) = squares.iter().enumerate().max_by_key(|&(_, l)| l).unwrap();
-        if *smax > max {
-            max = *smax;
-            let x = i as u32 % (300 - s + 1) + 1;
-            let y = i as u32 / (300 - s + 1) + 1;
+    for s in 1..=size {
+        let (i, smax) = (1..=size - s + 1)
+            .flat_map(|x| {
+                let lines: Vec<i32> = (1..=size)
+                    .map(|y| (0..s).map(|sx| get(&grid, x + sx, y)).sum())
+                    .collect();
+
+                (1..=size - s + 1)
+                    .map(move |y| lines.iter().skip((y - 1) as usize).take(s as usize).sum())
+            })
+            .enumerate()
+            .max_by_key(|&(_, l)| l)
+            .unwrap();
+
+        if smax > max {
+            max = smax;
+            let x = i as u32 % (size - s + 1) + 1;
+            let y = i as u32 / (size - s + 1) + 1;
             result = format!("{},{},{}", x, y, s);
         }
     }
